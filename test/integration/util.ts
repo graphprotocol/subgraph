166d94c5a242d09662390e2b306e8893cf7f352e
Oren <oren.sokolowsky@gmail.com>
2018-11-20 01:24:58 +0200

    graph node v 4.0.1

--- test/integration/util.ts
+++ test/integration/util.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 process.env = {
     ethereum: 'http://127.0.0.1:8545',
-    node_http: 'http://127.0.0.1:8000/daostack/graphql',
+    node_http: 'http://127.0.0.1:8000/by-name/daostack/graphql',
     node_ws: 'http://graph-node:8001/by-name/daostack',
     test_mnemonic:
         'behave pipe turkey animal voyage dial relief menu blush match jeans general',
